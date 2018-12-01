cc96a4bc99901d32807bd40f5cc1f7822cac3a2d
MR Bednar <M.R.Bednar@gmail.com>
2018-12-01 12:59:53 -0800

    Dot Net Inital Commit

---
+++ Advent2018NetCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Advent2018NetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
